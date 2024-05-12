c95a9f818327b019b155f6caf3b79ab8fcc1c3cf
sourabh-r-k <01fe22bca027@kletech.ac.in>
2024-05-12 12:54:33 +0530

    First commit for student

---
+++ Student.java
@@ -0,0 +1,21 @@
+public class Student{
+    String name;
+    int Birthyear;
+    public Student(String name , int Birthyear ){
+        this.name = name;
+        this.Birthyear = Birthyear;
+    }
+    public int  calculateage(int Birthyear){
+        return 2024 - Birthyear;
+    } 
+    public void dispaly(){
+        System.out.println("Name: " + name);
+        System.out.println("Age: "+calculateage(Birthyear));
+    }
+    public static void main(String[] args) {
+        Student s1=new Student("Ramkamal", 2005);
+        s1.calculateage(2005);
+        s1.dispaly();
+    }
+}
+
